6a683c60bf6e2d8d6c44483f9bb393c4c932e375
Robert Hammock <rhamm1027@gmail.com>
2021-03-30 17:05:38 -0400

    getting ready to start

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
-require('dotenv').config();
 
 const app = express();
 
--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
+const knex = require('knex');
 const app = require('./app');
-const {PORT} = require('./config');
+const { PORT, DATABASE_URL } = require('./config');
 const MODE = process.env.NODE_ENV;
+
+const db = knex({
+  client: 'pg',
+  connection: DATABASE_URL,
+});
+
+app.set('db', db);
 
 app.listen(PORT, () => {
   console.log(`Server running in ${MODE} mode and listening at port: ${PORT}`)
